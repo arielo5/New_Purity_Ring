bf70367f2410018e4a6046fe42c2da7361895509
Ariel Martinez <arielo5.gamer@gmail.com>
2021-07-24 10:24:40 -0500

    update the playerRoute, index, created a profileRoute and updated some handlebars to work with bulma

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,16 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const playerRoutes = require('./playerRoutes');
-// const coachRoutes = require('./coachRoutes');
-// const fanRoutes = require('./fanRoutes');
+const coachRoutes = require('./coachRoutes');
+// const fanRoutes = require('./fanRoutes'); 
+// const teamRoutes = require('./teamRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/player', playerRoutes);
-// router.use('/coach', coachRoutes);
+router.use('/coach', coachRoutes);
 // router.use('/fan', fanRoutes);
+// router.use('/team', teamRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
