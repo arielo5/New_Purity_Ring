955e48d39c9bb0043e6af1bd98ecb6700c27bb3b
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-07-27 20:20:41 -0500

    updated manageTeams, giving the coach the ability to update the stats

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const coachProfileRoutes = require('./coachProfileRoutes');
 const profileRoutes = require('./profileRoutes');
+const statsRoutes = require('./statsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/coach-profile', coachProfileRoutes);
 router.use('/profile', profileRoutes);
+router.use('/stats', statsRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,11 +3,50 @@ const sequelize = require('../config/connection');
 const { User, Team, Player, Coach, Fan } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
+    if (!req.session.is_coach) {
+      res.render('homepage', {
       loggedIn: req.session.loggedIn,
       is_coach: req.session.is_coach
     })
+  } else {
+  Coach.findOne({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'team_name',
+    ],
+    // include: [ 
+    //   {
+    //     model: User,
+    //     attributes: ['id', 'team_name', 'user_id']
+    //   }
+    // ]
+  })
+    .then(dbCoachData => {
+      // if (!dbCoachData) {
+      //   res.status(404).json({ message: 'No user found with this id' });
+      //   return;
+      // }
+
+      // serialize the data
+      const coach = dbCoachData.get({ plain: true });
+
+      res.render('homepage', {
+          coach,
+          loggedIn: req.session.loggedIn,
+          is_coach: req.session.is_coach
+          });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  };
   });
+
+
     
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
